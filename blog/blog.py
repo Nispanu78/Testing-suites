efa5ad6f13aacce43e5e0c82790c3baaf4610538
Nicola Spanu <nicspanu@gmail.com>
2020-10-05 11:03:25 +0200

    second commit

--- blog/blog.py
+++ blog/blog.py
@@ -1,3 +1,5 @@
+from blog.post import Post
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -10,6 +12,15 @@ class Blog:
                                               len(self.posts),
                                               's' if len(self.posts) != 1 else '')
 
+    def create_post(self, title, content):
+        self.posts.append(Post(title, content))
+
+    def json(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
+        }
 
 
 
