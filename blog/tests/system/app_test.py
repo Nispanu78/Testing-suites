64097909d3201d0707ebba8dd718350ba60c7b67
Nicola Spanu <nicspanu@gmail.com>
2020-10-06 11:06:21 +0200

    third commit

---
+++ blog/tests/system/app_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import patch
+from blog import appl
+from blog.blog import Blog
+
+class AppTest(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            appl.menu()
+            mocked_input.assert_called_with(appl.MENU_PROMPT)
+
+    def test_menu_prints_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                appl.menu()
+                mocked_print_blogs.assert_called()
+
+    def test_print_blogs(self):
+        blog = Blog('Test', 'Test Author')
+        appl.blogs = {'Test': blog}
+        with patch('builtins.print') as mocked_print:
+            appl.print_blogs()
+            mocked_print.assert_called_with('-Test by Test Author (0 posts)')
